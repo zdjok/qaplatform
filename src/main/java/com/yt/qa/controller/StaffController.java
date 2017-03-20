be9e6fc20c74001d95dafc1554690c5828db02f7
zdjok <zhengdejing_2001@163.com>
2017-03-20 20:13:56 +0800

    add

--- src/main/java/com/yt/qa/controller/StaffController.java
+++ src/main/java/com/yt/qa/controller/StaffController.java
@@ -2,12 +2,15 @@ package com.yt.qa.controller;
 
 import javax.annotation.Resource;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yt.qa.entity.StaffDO;
+import com.yt.qa.enumbean.StaffErrorInfoEnum;
+import com.yt.qa.errorhandler.GlobalErrorInfoException;
 import com.yt.qa.service.StaffService;
 
 /**
@@ -18,12 +21,16 @@ import com.yt.qa.service.StaffService;
 @RestController
 @RequestMapping("/staff")
 public class StaffController {
+	Logger logger = Logger.getLogger(StaffController.class);
 	
 	@Resource
 	StaffService staffService;
 	
 	@GetMapping("/{id}")
-	public StaffDO showStaff(@PathVariable("id") int id){
-		return staffService.selectStaffById(id);
+	public StaffDO showStaff(@PathVariable("id") int id) throws GlobalErrorInfoException{
+		StaffDO staff = staffService.selectStaffById(id);
+		if(staff == null)
+			throw new GlobalErrorInfoException(StaffErrorInfoEnum.STAFF_ID_INVALID);
+		return staff;
 	}
 }
